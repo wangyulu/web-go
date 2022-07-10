dbd42e4d8e9bf164184d13da3b487c59fcc1c26c
sky <972653707@qq.com>
2022-07-10 15:01:58 +0800

    六、优雅关闭

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/wangyulu/web-go/framework"
 	"github.com/wangyulu/web-go/framework/middleware"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -21,5 +27,22 @@ func main() {
 		Addr: ":8080",
 	}
 
-	server.ListenAndServe()
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	// 当前的goroutine等待信号量
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
 }
--- user_controller.go
+++ user_controller.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"github.com/wangyulu/web-go/framework"
+	"time"
 )
 
 func UserLoginController(c *framework.Context) error {
+	// 等待10s才结束执行
+	time.Sleep(10 * time.Second)
+
 	c.SetOkStatus().Json("ok, UserLoginController")
 	return nil
 }
